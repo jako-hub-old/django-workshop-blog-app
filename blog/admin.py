d48491f0797469281ed835632c09fdc07e7a0e6c
jakolab <alejo.jko@gmail.com>
2018-02-22 17:52:35 -0500

    Finished the list an detail posts

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):    
+    """Modelo de los posts...
+    Ejemplo de documentación de clases
+    Note: 
+        más información: https://www.python.org/dev/peps/pep-0257/
+    Attributes:
+        Attr1 ([type]): Description...
+        Attr2 (:obj:int, optional): Description...
+    """
     list_display = ('title', 'slug', 'author', 'publish', 'status',) # Permite asignar las columnas que se mostrarán en la vista de lista.
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
